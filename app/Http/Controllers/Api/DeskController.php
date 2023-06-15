0fecb7933a8754e7d602f2ad2fa1dfa2ecb17d6c
Rinat Sarmuldin <ura07srr@gmail.com>
2023-06-15 14:08:15 +0500

    update-delete info in db

--- app/Http/Controllers/Api/DeskController.php
+++ app/Http/Controllers/Api/DeskController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\DeskStoreRequest;
 use App\Http\Resources\DeskResource;
 use App\Models\Desk;
+use http\Client\Response;
 use Illuminate\Http\Request;
 
 class DeskController extends Controller
@@ -17,7 +18,7 @@ class DeskController extends Controller
      */
     public function index()
     {
-        return DeskResource::collection(Desk::with('lists')->get());
+        return DeskResource::collection(Desk::all());
     }
 
     /**
@@ -38,9 +39,9 @@ class DeskController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Desk $desk)
     {
-        return new DeskResource(Desk::with('lists')->findOrFail($id));
+        return new DeskResource(Desk::findOrFail($desk));
     }
 
     /**
@@ -50,9 +51,10 @@ class DeskController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(DeskStoreRequest $request, Desk $desk)
     {
-        //
+        $desk->update($request->validated());
+        return  new DeskResource($desk);
     }
 
     /**
@@ -61,8 +63,9 @@ class DeskController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Desk $desk)
     {
-        //
+        $desk->delete();
+        return response(null, \Illuminate\Http\Response::HTTP_NO_CONTENT);
     }
 }
